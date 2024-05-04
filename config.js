11cbdf73d0d02142ea80a201bbd69add2f390031
Ben Coveney <bencoveney@gmail.com>
2024-05-04 21:57:00 +0100

    Add 50 and 950

--- config.js
+++ config.js
@@ -7,9 +7,13 @@ function createConfig(stopCount, tintCount) {
   );
 
   // From 0 to 1000, excluding the endpoints
-  const tints = Array.from(Array(tintCount + 2))
-    .map((_, index) => lerp(0, 1000, index / (tintCount + 1)))
-    .slice(1, tintCount + 1);
+  const tints = [
+    50,
+    ...Array.from(Array(tintCount + 2))
+      .map((_, index) => lerp(0, 1000, index / (tintCount + 1)))
+      .slice(1, tintCount + 1),
+    950,
+  ];
 
   return {
     stops,
